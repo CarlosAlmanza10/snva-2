9f7155d8b9b2cf0430a7c2d6550ea54cd26ba419
CarlosAlmanza10 <carlos.almanza@sinova.co>
2025-01-14 15:04:16 -0500

    nuevos campos

--- academy/models/course.py
+++ academy/models/course.py
@@ -16,3 +16,14 @@ class Course(models.Model):
                              ],
                              copy=False)
     sesion_ids = fields.One2many(comodel_name='academy.session', string='Sessions', inverse_name='course_id')
+    currency_id = fields.Many2One(comodel_name="res.currency", string="Precio", default= lambda self:self.env.company.currency_id.id)
+    base_price = fields.Monetary(string="Precio base", currency_field= "currency_id")
+    additional_fee = fields.Monetary(string="Fee Adicional", currency_field= "currency_id")
+    total_price = fields.Monetary(string="Precio total", currency_field= "currency_id", compute="_compute_total_price")
+    
+    @api.depends("base_price", "additional_fee")
+    def _compute_total_price(self):
+        for record in self:
+            if(record.base_price < 0):
+                raise ValidationError(('EL precio no puede ser negativo'))
+            record.total_price = record.base_price + record.additional_fee
